2fd566ced667b30e2fc4a50a812f2ac1fef7ec10
Muflone <webreg@vbsimple.net>
2013-11-30 23:28:30 +0100

    Fixed the file paths for jplayer

--- resources/jplayer/create_new_jplayer.js
+++ resources/jplayer/create_new_jplayer.js
@@ -1,6 +1,6 @@
 var last_jplayer_index = 0;
 
-function create_new_jplayer(basefile, description) {
+function create_new_jplayer(resource, basefile, description) {
     last_jplayer_index++;
     var index=last_jplayer_index;
     // Create new player
@@ -9,15 +9,15 @@ function create_new_jplayer(basefile, description) {
     document.write('<a href="#" id="playButton' + index + '">' + description + '</a>');
     jQuery("#jquery_jplayer_" + index).jPlayer( {
         errorAlerts: true,
-        swfPath: '../resources/jplayer/Jplayer.swf',
+        swfPath: '../../resources/jplayer/Jplayer.swf',
         solution: 'flash, html',
         supplied : 'mp3, oga, m4a, wav',
         ready: function () {
           jQuery(this).jPlayer("setMedia", {
-            mp3: '../resources/demo/' + basefile + '/sample.mp3',
-            oga: '../resources/demo/' + basefile + '/sample.oga',
-            m4a: '../resources/demo/' + basefile + '/sample.m4a',
-            wav: '../resources/demo/' + basefile + '/sample.wav'
+            mp3: '../../resources/' + resource + '/demo/' + basefile + '/sample.mp3',
+            oga: '../../resources/' + resource + '/demo/' + basefile + '/sample.oga',
+            m4a: '../../resources/' + resource + '/demo/' + basefile + '/sample.m4a',
+            wav: '../../resources/' + resource + '/demo/' + basefile + '/sample.wav'
           });
         }
     });
